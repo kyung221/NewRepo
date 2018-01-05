a40d08209b9035db7df55f273519e44a9af6059e
kyung221 <roodwl@naver.com>
2018-01-05 17:49:37 +0900

    Update shell.sh

--- script/shell.sh
+++ script/shell.sh
@@ -1,6 +1,6 @@
-library=$1
-symbols="$*"
-symbols=${symbols#$1}
+library=$1 #첫번째 인자
+symbols="$*" #모든 인자
+symbols=${symbols#$1} #symbols앞에서부터 $1을 찾아 첫번째로 나타나는 패턴 버림 
 
 if [ ! -f $library ];
 then echo "library not exist";
@@ -14,7 +14,7 @@ echo "libraryname:" $library "symbol:" $symbolname ;
 return=$(./build/CheckSymbol ./$library $symbolname);
 
 if [ $? = 0 ]; 
-then echo "OK";
+then echo "OK";#최근 명령어의 종료 상태가 정상
 else echo "FAIL";
 fi
 
